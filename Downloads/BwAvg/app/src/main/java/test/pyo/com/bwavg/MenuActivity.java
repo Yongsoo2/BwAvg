47d6c1d6578cbdeb3b8476092e9a02025e39c61a
yongsoo <vydydtn2@naver.com>
2018-04-10 14:38:02 +0900

    bwAvg 로그인 (firebase 접속)

---
+++ Downloads/BwAvg/app/src/main/java/test/pyo/com/bwavg/MenuActivity.java
@@ -0,0 +1,13 @@
+package test.pyo.com.bwavg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
